f4ddcc90f723b50203126ec6215ceae8dba72232
Thilakgovind <redemption23to25@gmail.com>
2025-08-01 04:36:25 +0530

    !

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 print("Hello, Welcome to the game!")
+print("Welcome to the secret version!")
 
 import random
 
 def random_number():
-    return random.randint(1, 10)
+    return random.randint(1, 100)
 
 def main():
     while True:
-        g = int(input("Guess a number between 1 and 10 (or 0 to exit): "))
+        g = int(input("Guess a number between 1 and 100 (or 0 to exit): "))
         random_num = random_number()
         if g == 0:
             print("Exiting the game.")
             break
-        elif g < 1 or g > 10:
-            print("Please enter a valid number between 1 and 10.")
+        elif g < 1 or g > 100:
+            print("Please enter a valid number between 1 and 100.")
         elif g == random_num:
             print("Congratulations! You guessed the right number:", random_num)
             random_num = random_number()
